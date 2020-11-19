f8fa761347f080ba5ba00c94738a345536d2dc04
Ubuntu <ubuntu@ip-172-31-16-204.us-east-2.compute.internal>
2020-11-19 02:16:08 +0000

    Adds test

---
+++ tests/tests.py
@@ -0,0 +1,25 @@
+import requests
+
+def bet_one_test():
+    url = "http://ec2-3-134-244-81.us-east-2.compute.amazonaws.com:5000/bet"
+    data = {"1": 1.5, "X": 3.5, "2": 8, "bonus": 500}
+    response = requests.post(url, json=data).json()
+    test_data = {'bonus': 500.0,
+                 'crossBet': 500.0, 
+                 'oneBet': 2500, 
+                 'twoBet': 500, 
+                 'winningsOnCross': 0.0, 
+                 'winningsOnOne': 250.0, 
+                 'winningsOnTwo': 500.0
+                }
+    if response == test_data:
+        return 0
+    else:
+        print("Response:")
+        print(response)
+        print("Test data:")
+        print(test_data)
+        return 1
+
+if __name__ == '__main__':
+    bet_one_test()
