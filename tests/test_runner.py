28c9329d14cb45d6675b86ae69335d17a89439ed
Ubuntu <ubuntu@ip-172-31-16-204.us-east-2.compute.internal>
2020-11-19 10:10:16 +0000

    Imports requests

--- tests/test_runner.py
+++ tests/test_runner.py
@@ -1,14 +1,16 @@
 import sys
+import requests
 from bet_test import bet_one_test, bet_two_test
 from log_module import write_log_file, print_log, append_to_log
 
-URL = "http://localhost:5000/bet"
+url = "http://" + requests.get("http://169.254.169.254/latest/meta-data/public-hostname").text + ":5000/bet"
+
 TESTS = [bet_one_test, bet_two_test]
 log = "" 
 
 if __name__ == '__main__':
     for test in TESTS:
-        result = test(URL)
+        result = test(url)
         print_log(result)
         log = append_to_log(log, result)
     if log == "":
