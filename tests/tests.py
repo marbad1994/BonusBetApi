f6df82cce83061b8f280f430013b55a1b63fb464
Ubuntu <ubuntu@ip-172-31-16-204.us-east-2.compute.internal>
2020-11-19 04:37:40 +0000

    Removes tests from docker

--- tests/tests.py
+++ tests/tests.py
@@ -1,7 +1,10 @@
 import requests
+import datetime
+import sys
 
 def bet_one_test():
     url = "http://ec2-18-225-6-54.us-east-2.compute.amazonaws.com:5000/bet"
+    url = "http://172.31.16.204:5000/bet"
     data = {"1": 1.5, "X": 3.5, "2": 8, "bonus": 500}
     response = requests.post(url, json=data).json()
     test_data = {'bonus': 500.0,
@@ -14,13 +17,15 @@ def bet_one_test():
                  'winningsOnTwo': 500.0
                 }
     if response == test_data:
-        return 0
+        sys.exit(0)
     else:
         print("Response:")
         print(response)
         print("Test data:")
         print(test_data)
-        return 1
+        with open("log" + datetime.datetime.now(), "w") as f:
+            f.write("Response:\n" + response + "\nTest data:\n" + test_data)
+        sys.exit(1)
 
 if __name__ == '__main__':
     bet_one_test()
